5c364535658e8fbd20d7f29f0c5baad578dc6b16
amol <amol.jagdale@neosoftmail.com>
2022-06-16 12:59:10 +0530

    Added object detection in acne classification

--- app.py
+++ app.py
@@ -1,41 +1,27 @@
 import os
 import uuid
-import flask
 import urllib
-from flask import Flask, render_template, request,redirect
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import tensorflow as tf
-
-import glob
+from flask import Flask, render_template, request, redirect
 import cv2
-from tensorflow import keras
+from detect import pre_process, post_process
 import numpy as np
 
-skin_model = keras.models.load_model("skin_disease_model.h5", compile=True)
-
-LABEL_MAP = ['Acne and Rosacea Photos',
-             'Actinic Keratosis Basal Cell Carcinoma and other Malignant Lesions',
-             'Eczema Photos',
-             'Melanoma Skin Cancer Nevi and Moles',
-             'Psoriasis pictures Lichen Planus and related diseases',
-             'Tinea Ringworm Candidiasis and other Fungal Infections',
-             'Urticaria Hives',
-             'Nail Fungus and other Nail Disease']
+modelWeights = 'acne_classification_v1.onnx'
+net = cv2.dnn.readNet(modelWeights)
+classes = ['Level_0', 'Level_1', 'Level_2']
 
 
-def detect(filename):
-    img = load_img(filename, target_size=(32, 32))
-    img = img_to_array(img)
-
-    image = cv2.resize(img, (32, 32)) / 255.0
-    prediction = skin_model.predict(image.reshape(1, 32, 32, 3), verbose=0)
-    max_score = prediction[0][np.argsort(prediction[0])][::-1][:3]*100
-    max_class = np.argsort(prediction[0])[::-1][:3]
-    max_score = np.around(max_score, decimals=2)
-    out_class = []
-    for ind in max_class:
-        out_class.append(LABEL_MAP[ind])
-    return out_class, max_score
+def detect(filename, resource):
+    # read image from
+    f = resource.read()
+    npimg = np.fromstring(f, np.uint8)
+    frame = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+    # Process image.
+    detections = pre_process(frame, net)
+    img, result = post_process(frame.copy(), detections)
+    # save image to load in front end
+    cv2.imwrite(filename, img)
+    return img
 
 
 app = Flask(__name__)
@@ -59,69 +45,39 @@ def success():
     error = ''
     target_img = os.path.join(os.getcwd(), 'static/images')
     if request.method == 'POST':
-        if (request.form):
+        if request.form:
             link = request.form.get('link')
             try:
                 resource = urllib.request.urlopen(link)
                 unique_filename = str(uuid.uuid4())
                 filename = unique_filename + ".jpg"
                 img_path = os.path.join(target_img, filename)
-                output = open(img_path, "wb")
-                output.write(resource.read())
-                output.close()
+                img = detect(img_path, resource)
                 img = filename
-
-                class_result, prob_result = detect(img_path)
-
-                predictions = {
-                    "class1": class_result[0],
-                    "class2": class_result[1],
-                    "class3": class_result[2],
-                    "prob1": prob_result[0],
-                    "prob2": prob_result[1],
-                    "prob3": prob_result[2],
-                }
-
             except Exception as e:
                 print(str(e))
                 error = 'This image from this site is not accesible or inappropriate input'
 
-            if (len(error) == 0):
-                print(predictions)
-                return render_template('success.html', img=img, predictions=predictions)
+            if len(error) == 0:
+                return render_template('success.html', img=img)
             else:
-                return redirect('/', error=error)
+                return redirect('/')
 
-
-        elif (request.files):
+        elif request.files:
             file = request.files['file']
             if file and allowed_file(file.filename):
-                file.save(os.path.join(target_img, file.filename))
                 img_path = os.path.join(target_img, file.filename)
                 img = file.filename
-
-                class_result, prob_result = detect(img_path)
-
-                predictions = {
-                    "class1": class_result[0],
-                    "class2": class_result[1],
-                    "class3": class_result[2],
-                    "prob1": prob_result[0],
-                    "prob2": prob_result[1],
-                    "prob3": prob_result[2],
-                }
-
+                detect(img_path, file)
             else:
                 error = "Please upload images of jpg , jpeg and png extension only"
-
-            if (len(error) == 0):
-                return render_template('success.html', img=img, predictions=predictions)
+            if len(error) == 0:
+                return render_template('success.html', img=img)
             else:
-                return redirect('/', error=error)
-
+                return redirect('/')
     else:
-        return redirect('/', error=error)
+        return redirect('/')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5020)
